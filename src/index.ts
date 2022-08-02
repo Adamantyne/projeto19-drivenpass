59504c16d100531b5e620455efce5a66a1913675
Luana Lessa <lessalsn@gmail.com>
2022-08-02 13:34:52 -0300

    Code review

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-import app from "./app.js";
-
-const port:number = +process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(chalk.blue(`Server is up on port: ${port}`));
-});
